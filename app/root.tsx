2abe1895a8a648cdb17299a3e8ec63d53643369e
anas <anas.khan@reveation.io>
2023-06-15 17:28:27 +0530

    Add:fav icon

--- app/root.tsx
+++ app/root.tsx
@@ -115,6 +115,11 @@ export function links() {
       href: styles,
     },
     {
+      rel: 'icon',
+      href: '/favicon-16x16.png',
+      type: 'image/png',
+    },
+    {
       href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap',
       rel: 'stylesheet',
       as: 'font',
