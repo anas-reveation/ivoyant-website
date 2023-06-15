51a8397ba4e27183cf508c69b4f5b9a27d5ee863
Ayush <srivastava.ayush582@gmail.com>
2023-06-15 18:54:10 +0530

    fix:lighthouse issue

--- app/root.tsx
+++ app/root.tsx
@@ -55,6 +55,7 @@ export default function App() {
         <LiveReload />
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+          defer
           integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
           crossOrigin="anonymous"
         ></script>
